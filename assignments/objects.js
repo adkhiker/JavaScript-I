21be7c0948a956b8cd4a591c5c63738cffa2d0a9
adkhiker <poirier.greg@gmail.com>
2019-06-10 15:40:44 -0400

    WIP: intern objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,19 +18,51 @@ const example = {
 };
 
 // Write your intern objects here:
+const mitzi = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+};
+const kennan = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M"
+};
+const keven = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+};
+const antonietta = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F"
+};
+
+
 
 // ==== Challenge 2: Reading Object Data ====
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+
+
 // Kennan's ID
+
 
 // Keven's email
 
+
 // Gannie's name
 
+
 // Antonietta's Gender
+
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
